5ade04ec7f7b0df9652ca7a0c2e45324e00adb8e
eserven <emily.e.serven@gmail.com>
2025-11-22 16:43:48 -0500

    Extracted computerRoll method

--- packages/client/src/routes/index.tsx
+++ packages/client/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 
 import { GameButton } from "@/components/GameButton";
+import { computerRoll } from "@/lib/computerRoll";
 import { gameCheck } from "@/lib/gameCheck";
 
 export const Route = createFileRoute("/")({
@@ -23,29 +24,6 @@ function Index() {
       return ["The result is a tie!", "tie"];
     }
     return gameCheck[choice1][choice2];
-  };
-
-  const computerRoll = function () {
-    console.log("2. Computer is rolling.");
-    const computerChoice = Math.random();
-    let computerReturn = "";
-    // Above will select a random number between 0 and 1 and store that number in the computerChoice variable.
-    // Below if statement will assign Rock, Paper, or Scissors to the number.
-    if (computerChoice < 0.34) {
-      computerReturn = "rock";
-      console.log("3a. Math is " + computerChoice);
-    }
-    else if (computerChoice <= 0.67) {
-      computerReturn = "paper";
-      console.log("3b. Math is " + computerChoice);
-    }
-    else {
-      computerReturn = "scissors";
-      console.log("3c. Math is " + computerChoice);
-    }
-    console.log("3sub. Computer Choice is " + computerChoice);
-    // Return the string as the variable computerChoice.
-    return computerReturn;
   };
 
   const userChoiceFunc = function (choice: string) {
