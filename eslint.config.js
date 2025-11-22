a76e52ccf520d08fd884399dd7316961d120b1af
eserven <emily.e.serven@gmail.com>
2025-11-22 17:08:21 -0500

    Use improved eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -4,28 +4,4 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config([
   ...emstackConfig,
-  {
-    files: ["**/*.{ts,tsx}"],
-    ignores: [
-      "packages/middleware/src/**/*.ts",
-      "packages/**/src/**/*.test.{js,ts}",
-      "packages/client/server.js",
-    ],
-    rules: {
-      "no-unused-vars": "off",
-
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          args: "all",
-          argsIgnorePattern: "^_",
-          caughtErrors: "all",
-          caughtErrorsIgnorePattern: "^_",
-          destructuredArrayIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          ignoreRestSiblings: true,
-        },
-      ],
-    },
-  },
 ]);
