6624776186e0e4f37ebc99aab385060e2e7098e2
eserven <emily.e.serven@gmail.com>
2025-11-22 16:40:47 -0500

    Update lint config - make it so no-unused-vars works with typescript

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,22 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config([
   ...emstackConfig,
+  {
+    rules: {
+      "no-unused-vars": "off",
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
 ]);
