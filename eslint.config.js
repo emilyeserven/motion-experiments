ca7787308a1417aab12db79895b3daee12d3923d
eserven <emily.e.serven@gmail.com>
2025-11-24 17:48:49 -0500

    Fixed issues that were making linting fail

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,11 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config([
   ...emstackConfig,
+  {
+    settings: {
+      "better-tailwindcss": {
+        entryPoint: "./packages/client/src/index.css",
+      },
+    },
+  },
 ]);
